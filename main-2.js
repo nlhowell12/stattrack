05736bad888c307757872a98dfb11a1ebb044758
Nick Howell <nlhowell12@gmail.com>
2018-03-08 14:26:52 -0500

    added click listener

---
+++ main-2.js
@@ -0,0 +1,82 @@
+const state = {
+	players: {
+
+    }
+}
+
+function createPlayer(name) {
+    state.players[name] = {
+        stunned: 0,
+        poisoned: 0,
+        charmed: 0,
+        fatigued: 0,
+        frightened: 0,
+        blinded: 0,
+        paralyzed: 0,
+    }
+}
+
+function addPlayerToState () {
+    let charName = document.getElementById("charInput")
+    createPlayer(charName.value);
+    charName.value = '';
+    drawPlayers();
+}
+
+// Adds an event listener to every button
+var buttons = document.getElementsByTagName("button");
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", buttonClick);
+}
+
+function buttonClick() {
+    let clickEvent = event.target;
+    console.log(clickEvent.id)
+    console.log(clickEvent.className)
+    let button = document.getElementsByTagName("button");
+    if (clickEvent.className == "effect_but") {
+        addEffect(charName, clickEvent.id, length)
+    }
+}
+
+function addEffect(charName, effectName, length) {
+    state.players.charName[effectName] = length;
+}
+function drawPlayers() {
+	for (let player in state.players) {
+        drawPlayer(player);
+    }
+}
+
+function drawPlayer(playerName) {
+	let playerElement = document.getElementById(playerName);
+    const player = state.players[playerName];
+    
+    if (!playerElement) {
+        playerElement = document.createElement("div");
+        playerElement.id = playerName;
+        playerElement.classList.add("player");
+        let findDisplay = document.getElementById('display');
+        findDisplay.appendChild(playerElement);
+        for (let effect in player) {
+            let effectElement = document.createElement("div");
+            effectElement.classList.add("effect", effect);
+            effectElement.textContent = player[effect];
+            playerElement.appendChild(effectElement);
+        }
+    }
+    
+	for (let effect in player) {
+        const currentEffect = document.querySelector("#" + playerName + " > .effect." + effect);
+        currentEffect.style.width = (player[effect] * 50) + "px";
+        currentEffect.style.display = player[effect] ? "block" : "none";
+    }
+}
+
+function log(value) {
+	const div  = document.createElement("div");
+    div.textContent = JSON.stringify(value);
+    document.display.appendChild(div);
+}
+
+drawPlayers();
