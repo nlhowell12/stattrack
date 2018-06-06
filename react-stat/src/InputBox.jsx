99efd5da11f0fd869f936733b420709d0a64c1f4
Nick Howell <nlhowell12@gmail.com>
2018-06-06 15:47:40 -0400

    split out the input box

---
+++ react-stat/src/InputBox.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class InputBox extends Component {
+    render() {
+        return (
+<section id="input_box">
+    <section id="char_text">
+        <input id='charInput' type="text" placeholder={"Enter Character Name"} value={this.props.newPlayer} onChange={evt => {this.props.changeState({newPlayer: evt.target.value})}}/>
+        <input type="number" id="initInput" placeholder={"Initiative"} value={this.props.newPlayerInit} onChange={evt => {this.props.changeState({newPlayerInit: Number(evt.target.value)})}}/>
+        <button id='charSubmit' onClick={this.props.addPlayer(this.props.newPlayer, this.props.newPlayerInit)}>Submit</button>
+    </section>
+    <section id="turn_counter">
+        <p>Turn Counter</p>
+        <div id="turnDisplay"></div>
+        <button id="turnClick" >Next Turn</button>
+    </section>
+</section>
+        )
+    }
+}
+
+export default InputBox;
